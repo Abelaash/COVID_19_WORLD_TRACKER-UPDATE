56c36b468d103c05cf171a7baca9748d29aaf503
Abelaash <abelaash.giritharan@gmail.com>
2020-09-11 13:15:17 -0400

    ADD FILES TO REPO

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:covid_19_tracker/screens/live_tracker.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'COVID PANDEMIC WORLD TRACKER',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        textTheme: GoogleFonts.poppinsTextTheme(),
+      ),
+      debugShowCheckedModeBanner: false,
+      home: Tracker(),
+    );
+  }
+}
